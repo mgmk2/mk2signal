012d70017c36a8cf46c45dc60bb45dcadd3123be
mgmk2 <xis.t65@gmail.com>
2019-06-15 23:37:58 +0900

    simplify code about multi dimension

--- STFT.py
+++ STFT.py
@@ -6,58 +6,37 @@ class STFT(object):
         self.fmax = self.wlen // 2
         self.strides = 0.5
         self.noverlap = int(self.wlen * self.strides)
-        if window == 'hann':
-            self.wf = 0.5 - 0.5 * np.cos(2 * np.pi * np.arange(self.wlen) / self.wlen)
+        self.window = window
+
+    def get_window(self, dtype=None):
+        self.window == 'hann':
+            wf = 0.5 - 0.5 * np.cos(2 * np.pi * np.arange(self.wlen) / self.wlen)
         else:
-            self.wf = self.ones([self.wlen])
+            wf = self.ones([self.wlen])
+        return wf
 
     def transform(self, x, dtype=None):
-        # reshape x to 2D
-        shape_x = x.shape
-        if shape_x > 1:
-            # dimension of x is 2 or more
-            x = x.reshape([-1, shape_x[-1]])
-            n = x.shape[0]
-            shape_y = shape_x[:-1]
-        else:
-            # dimension of x is 1
-            x = x.reshape([1, -1])
-            n = 1
-            shape_y = []
-
         # set dtype
         if dtype is None:
             dtype = x.dtype
 
-        frame = (shape_x[-1] - self.wlen) // self.noverlap + 1
-        xx = np.zeros([n, frame, self.wlen], dtype=dtype)
+        # get window func
+        wf = self.get_window(dtype=dtype)
+
+        frame = (x.shape[-1] - self.wlen) // self.noverlap + 1
+        xx = np.zeros([*x.shape[:-1], frame, self.wlen], dtype=dtype)
         for tt in range(frame):
-            xx[:, tt, :] = x[:, tt * self.noverlap:tt * self.noverlap + self.wlen]
-        xx = xx.reshape(shape_y + [frame, self.wlen])
-        y = np.fft.rfft(xx * self.wf)
+            xx[..., tt, :] = x[..., tt * self.noverlap:tt * self.noverlap + self.wlen]
+        y = np.fft.rfft(xx * wf)
         return y
 
     def transform_inverse(self, y, dtype=None):
-        # reshape x to 2D
-        shape_y = y.shape
-        if shape_y > 2:
-            # dimension of x is 2 or more
-            y = y.reshape([-1, *shape_y[-2:]])
-            n = y.shape[0]
-            shape_x = shape_y[:-2]
-        else:
-            # dimension of x is 1
-            y = y[np.newaxis, :, :]
-            n = 1
-            shape_x = []
-
         # set dtype
         if dtype is None:
-            dtype = x.dtype
+            dtype = y.dtype
 
         frame = y.shape[-2]
-        x = np.zeros([n, (frame + 1) * self.noverlap], dtype=dtype)
+        x = np.zeros([*y.shape[:-2], (frame + 1) * self.noverlap], dtype=dtype)
         for tt in range(frame):
-            x[:, tt * self.noverlap:tt * self.noverlap + self.wlen] += np.fft.irfft(y[:, tt, :])
-        x = x.reshape(shape_x + [(frame + 1) * self.noverlap])
+            x[..., tt * self.noverlap:tt * self.noverlap + self.wlen] += np.fft.irfft(y[..., tt, :])
         return x
