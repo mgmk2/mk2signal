adb67dd66cc03615e71e38b07b884422ef65814b
mgmk2 <xis.t65@gmail.com>
2019-08-18 23:55:03 +0900

    organize some methods

--- wavelet/CWavelet.py
+++ wavelet/CWavelet.py
@@ -13,11 +13,11 @@ def fft_convolve(x1, x2):
 
 class CWavelet(object):
     def __init__(self, fs, wavelet='morlet', w0=6.0, window_len=None,
-        freq=None, freq_scale='log', n_freq=None, interval_freq=None):
+        freq_range=None):
 
         self.fs = fs # sampling frequency
         self.dt = 1 / self.fs # sampling interval
-        self.df = 1 / 12
+        self.n_per_octave = 12
         self.window_len_init = window_len
         self.window_len = window_len
         self.W = None
@@ -26,45 +26,24 @@ class CWavelet(object):
         else:
             raise ValueError('Unexpected wavelet: ' + str(wavelet))
 
-        if freq is None:
-            freq = (13.75, self.fs // 2)
+        if freq_range is None:
+            freq_range = (13.75, self.fs // 2)
         else:
-            freq = (max(0, freq[0]), min(self.fs // 2, freq[1]))
-
-        self.freq_period = self.get_freq_period(
-            freq, freq_scale=freq_scale, n_freq=n_freq, interval=interval_freq)
+            freq_range = (max(0, freq_range[0]), min(self.fs // 2, freq_range[1]))
+        self.freq_period = self.get_freq_period(freq_range)
         self.scale = self.wavelet.freq2scale(self.freq_period)
 
-    def _get_freq_log(self, freq):
+    def _get_freq_log(self, freq_range):
         # get frequencies spaced at epual intervals on log scale
         # frequencies on log scale is calculated from music keys
-        k0 = int(12 * np.log2(freq[0] / 440)) # minimum key
-        kn = int(12 * np.log2(freq[1] / 440)) # maximum key
-        freq_period = np.zeros([kn - k0 + 1], dtype=np.float64) # initialize
-        for i, k in enumerate(range(k0, kn + 1)):
-            freq_period[i] = 2 ** (k * self.df) * 440 # frequency at each key
+        k0 = int(self.n_per_octave * np.log2(freq_range[0] / 440)) # minimum key
+        kn = int(self.n_per_octave * np.log2(freq_range[1] / 440)) # maximum key
+        k = np.arange(k0, kn + 1, dtype=np.float64)
+        freq_period = 440 * 2 ** (k / self.n_per_octave) # frequency
         return freq_period
 
-    def _get_freq_linear(self, freq, n_freq=None, interval=None):
-        # get frequencies spaced at epual intervals on linear scale
-        if interval is not None and n_freq is not None:
-            # Error
-            pass
-        elif interval is None and n_freq is None:
-            # interval is 1
-            n_freq = freq[1] - freq[0] + 1
-        elif interval is not None:
-            # calc nfreq from interval
-            n_freq = np.ceil((freq[1] - freq[0] + 1) // interval) # ceil
-
-        freq_period = np.linspace(freq[0], freq[1], 2 * n_freq + 1, dtype=np.float64)[1::2]
-        return freq_period
-
-    def get_freq_period(self, freq, n_freq=None, interval=None):
-        if freq_scale == 'log':
-            freq_period = self._get_freq_log(freq)
-        elif freq_scale == 'linear':
-            freq_period = self._get_freq_linear(freq, n_freq=n_freq, interval=interval)
+    def get_freq_period(self, freq_range):
+        freq_period = self._get_freq_log(freq_range)
         return freq_period
 
     def _get_window_len(self, n):
@@ -78,31 +57,12 @@ class CWavelet(object):
         y = fft_convolve(x[..., np.newaxis, :], self.W[..., ::-1].conj())
         return y[..., ::strides]
 
-    def _transform_directly(self, x, strides):
-        nw = self.W.shape[-1]
-        p = (x.ndim - 1) * [(0, 0)] + [(nw // 2, nw // 2)]
-        x2 = np.pad(x, p, mode='constant')
-        n2 = x2.shape[-1]
-        num_strides = (n2 - nw) // strides + 1
-        y = np.zeros([*list(x.shape[:-1]), self.W.shape[0], num_strides], np.complex128)
-        for i in range(num_strides):
-            xi = x2[..., np.newaxis, i * strides:i * strides + nw]
-            y[..., :, i] = np.sum(xi.astype(np.complex128) * self.W, axis=-1)
-        return y
-
     def _transform(self, x, strides):
-        Nmax = max(x.shape[-1], self.W.shape[-1])
-        Nmin = min(x.shape[-1], self.W.shape[-1])
-        cost_fft = Nmax * np.log(Nmax)
-        cost_direct = Nmin * max(1, x.shape[-1] // strides)
-        if cost_fft < cost_direct:
-            y = self._transform_via_fft(x, strides)
-        else:
-            y = self._transform_directly(x, strides)
+        y = self._transform_via_fft(x, strides)
         return y
 
     def _update_W(self, N):
-        window_len = self._get_window(N)
+        window_len = self._get_window_len(N)
         if self.W is None or self.window_len != window_len:
             self.window_len = window_len
             self.n = np.arange(-window_len // 2, (window_len - 1) // 2)
@@ -111,13 +71,12 @@ class CWavelet(object):
 
     def transform(self, x, strides=1):
         self._update_W(x.shape[-1])
-        # wavelet transform
-        y = self._transform(x, strides)
+        y = self.dt * self._transform(x, strides)
         return y
 
     def transform_inverse(self, y):
         self._update_W(y.shape[-1])
-        C = self.wavelet.C
-        xs = fft_convolve(y, self.W)
-        x = 2 * np.log(2) * self.df / C * np.sum(xs.real, axis=-2)
+        xs = fft_convolve(y, self.W) / self.scale.reshape([-1, 1])
+        coefficient = 2 * np.log(2) * self.dt / self.n_per_octave / self.wavelet.C
+        x = coefficient * np.sum(xs.real, axis=-2)
         return x
--- wavelet/Morlet.py
+++ wavelet/Morlet.py
@@ -4,19 +4,39 @@ class Morlet(object):
     def __init__(self, w0=6.0):
         self.w0 = w0
         self.Cw = (1 + np.exp(-self.w0 ** 2) - 2 * np.exp(-0.75 * self.w0 ** 2)) ** (-0.5)
+        self.C = self._get_C()
+
+    def _get_C(self):
+        # calc admissible constant
+        wmax = 1000
+        dw = 0.01
+        w = np.arange(-wmax, wmax + 1, dw)
+        Wf = self._get_W_fourier(w)
+        C = 2 * np.pi * dw * np.sum(np.abs(Wf) ** 2 / np.abs(w))
+        return C
+
+    def _get_W_fourier(self, w):
+        # calc fourier transform of wavelet
+        k = np.exp(-0.5 * self.w0 ** 2)
+        Wf = self.Cw / np.pi ** 0.25 *\
+            (np.exp(-0.5 * (w - self.w0) ** 2) - k * np.exp(-0.5 * w ** 2))
+        return Wf
 
     def _get_W(self, t):
+        # calc wavelet
         k = np.exp(-0.5 * self.w0 ** 2)
         W = self.Cw / np.pi ** 0.25 *\
             np.exp(-0.5 * t ** 2) * (np.exp(1j * self.w0 * t) - k) # [n, s]
         return W
 
     def get_W(self, n, s):
-        t = n.reshape([1, -1])/ s.reshape([-1, 1])
-        W = self._get_W(t)
+        # calc wavelet at scale s
+        t = n.reshape([1, -1]) / s.reshape([-1, 1])
+        W = self._get_W(t) / s.reshape([-1, 1]) ** 0.5
         return W
 
     def freq2scale(self, freq):
+        # calc scales from frequencies.
         # center freq wf is given by the solution of below:
         #   wf = w0 / (1 - exp(-wf * w0))
         # if w0 > 5, wf is nearly equal to w0.
